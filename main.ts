109461adf08559b86dde89b186e3717076c6fd22
Jacky Chui <jacky.chui.smarthon@gmail.com>
2024-04-17 15:34:27 +0800

    add... fix...

--- main.ts
+++ main.ts
@@ -1,37 +1,36 @@
 /**
-* 使用此文件来定义自定义函数和图形块。
-* 想了解更详细的信息，请前往 https://makecode.microbit.org/blocks/custom
-*/
-
-enum MyEnum {
-    //% block="one"
-    One,
-    //% block="two"
-    Two
-}
-
-/**
- * Custom blocks
+ * Test block
  */
-//% weight=100 color=#0fbc11 icon=""
-namespace custom {
-    /**
-     * TODO: describe your function here
-     * @param n describe parameter here, eg: 5
-     * @param s describe parameter here, eg: "Hello"
-     * @param e describe parameter here
-     */
-    //% block
-    export function foo(n: number, s: string, e: MyEnum): void {
-        // Add code here
-    }
+//% weight=999 color=#3CB371 icon="" block="Test"
+namespace test {
+    let battery_level = 0
 
-    /**
-     * TODO: describe your function here
-     * @param value describe value here, eg: 5
-     */
-    //% block
-    export function fib(value: number): number {
-        return value <= 1 ? value : fib(value -1) + fib(value - 2);
+    //% blockId="smarthon_get_battery_level"
+    //% block="Get battery level (percentage) at Pin %pin"
+
+    export function getBattery(pin: AnalogPin): number {
+        let max = 660;   // define the max reading from battery
+        let min = 560;   // define the min reading from battery
+        let sum = 0;
+
+        //  read 30 times to get the average
+        for (let n = 0; n < 30; n++) {
+            sum += pins.analogReadPin(pin);
+            basic.pause(10);
+        }
+        let avg = sum / 30;
+
+        if (avg > min && avg < max) {
+            battery_level = Math.ceil(
+                pins.map(avg, min, max, 0, 100) / 5) * 5;
+            //  round up to multiple of 5
+        }
+        else if (avg > max) {
+            battery_level = 100;
+        }
+        else {
+            battery_level = 0;
+        }
+        return battery_level;
     }
 }
